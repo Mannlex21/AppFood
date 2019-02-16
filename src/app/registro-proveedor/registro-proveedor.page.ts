b638ee3c293b821a6dbf58d638f34f7b553f7e49
mannlex21 <mannlexx21@gmail.com>
2019-02-16 00:40:50 -0600

    trabajando con grids

--- src/app/registro-proveedor/registro-proveedor.page.ts
+++ src/app/registro-proveedor/registro-proveedor.page.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-proveedor.page.scss'],
 })
 export class RegistroProveedorPage implements OnInit {
-
+  proveedor = [{nombre: 'Cubanas'}, {nombre: 'hola'}];
+  hero = 'Windstorm';
   constructor() { }
 
   ngOnInit() {
