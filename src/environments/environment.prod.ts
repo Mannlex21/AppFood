dcd7ef87ff523090d808edee5d45cc43d8495805
mannlex21 <mannlexx21@gmail.com>
2019-02-17 23:45:34 -0600

    Se agrego variable de firebase a environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDwR2voBKKcD4SafzQTlYnbvRXG1XDtCq8',
+    authDomain: 'appfood-9cd15.firebaseapp.com',
+    databaseURL: 'https://appfood-9cd15.firebaseio.com',
+    projectId: 'appfood-9cd15',
+    storageBucket: 'appfood-9cd15.appspot.com',
+    messagingSenderId: '969940874616'
+  }
 };
