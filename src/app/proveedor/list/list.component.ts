432ab8d9d15e75147a92c25f7322132d736e08a4
mamurillo <mn.murillo@mavi.mx>
2019-02-18 19:01:51 -0600

    sE TRABAJO EN IMG

--- src/app/proveedor/list/list.component.ts
+++ src/app/proveedor/list/list.component.ts
@@ -5,6 +5,7 @@ import { Navigate } from 'src/app/store/router.state';
 import { Observable, Subscription } from 'rxjs';
 import { AppState } from 'src/app/store/app.state';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'list-proveedor',
@@ -14,7 +15,7 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class ListComponent implements OnInit {
   @Select(state => state.app) app$;
   state$: Observable<AppState>;
-  proveedor = [
+  proveedor= []; /*[
     {nombre: 'Cubanas', src: 'https://goo.gl/VbKRyZ'},
     {nombre: 'McDonald\'s '},
     {nombre: 'Burguer King'},
@@ -23,22 +24,31 @@ export class ListComponent implements OnInit {
     {nombre: 'Lirul Cisa'},
     {nombre: 'Tacos de Smog'},
     {nombre: 'DonJoJo'}
-  ];
+  ];*/
   items: Array<any>;
 
-  constructor(private store: Store, private db: AngularFireDatabase) {
+  constructor(private store: Store, private db: AngularFireDatabase,private sanitizer: DomSanitizer) {
     this.state$ = this.store.select(state => state);
   }
 
   ngOnInit() {
-    this.proveedor.forEach(element => {
-      if (element.src === undefined) {
-        element.src = 'https://goo.gl/jhsD4G';
-      }
-    });
+    // this.proveedor.forEach(element => {
+    //   if (element.src === undefined) {
+    //     element.src = 'https://goo.gl/jhsD4G';
+    //   }
+    // });
     // AQUI SE HACE LA CONSULTA A LA BD FIREBASE PARA OBTENER DATOS
     this.db.list('/proveedor').valueChanges().subscribe(d => {
       console.log(d);
+      // this.proveedor = d;
+      d.forEach(element => {
+        console.log(element['src'])
+        if (element['src'] != undefined) {
+          element['src'] = 'https://goo.gl/jhsD4G';
+        }
+        this.proveedor.push(element);
+      });
+
     });
   }
 
@@ -47,5 +57,9 @@ export class ListComponent implements OnInit {
       new SetUsername(username),
       new SetShowForm(true),
     ]);
+  }
+  getImg(val){
+    console.log(this.sanitizer.bypassSecurityTrustStyle('url(' + val + ')'))
+    return this.sanitizer.bypassSecurityTrustStyle('url(' + val + ')');
   }
 }
